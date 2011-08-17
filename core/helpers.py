1855de743e2589ccd3f19b89f1820a1c3f875c2f
Andy <amastbaum@gmail.com>
2011-08-17 16:27:12 -0400

    node discovery moved to function, dirt.py now has modes selectable on the command line

--- core/helpers.py
+++ core/helpers.py
@@ -1,3 +1,74 @@
+# these should be popped to different modules eventually
+
+# put in config file
+couchdb_host = 'http://localhost:5984'
+couchdb_dbname = 'dirt-kanso'
+
+def connect_to_db(host=couchdb_host, dbname=couchdb_dbname):
+    import couchdb
+    couch = couchdb.Server(couchdb_host)
+    try:
+        if couch.version() < '1.1.0':
+            print 'dirt: couchdb version >= 1.1.0 required'
+            sys.exit(1)
+        db = couch[couchdb_dbname]
+    except Exception:
+        print 'dirt: error connecting to database'
+        sys.exit(1)
+    print 'dirt: connected to db at %s/%s' % (couchdb_host, couchdb_dbname)
+    return db
+
+# this should be a couchdb view
+def get_nodes(db):
+    nodedocs = []
+    hostnames = []
+    for id in db:
+        try:
+            if db[id]['type'] == 'slave':
+                nodedocs.append(db[id])
+                hostnames.append(db[id]['hostname'])
+        except KeyError:
+            pass
+    return hostnames, nodedocs
+
+def node_recon(nodes, db, interactive=True):
+    import execnet
+    # move to settings.py
+    node_enable_default = True
+    node_password_default = 'pw123'
+
+    hostnames, nodedocs = get_nodes(db)
+    from tasks import system_info
+    for node in nodes:
+        try:
+            gw = execnet.makegateway('ssh=%s' % node)
+        except execnet.HostNotFound:
+            print 'dirt: host not found:', node
+            continue
+        print 'dirt: connected to host', node
+        ch = gw.remote_exec(system_info)
+        sys_info = ch.receive()
+
+        # update the db
+        if sys_info['hostname'] in hostnames:
+            d = nodedocs[hostnames.index(sys_info['hostname'])]
+            d['sys_info'] = sys_info
+        else:
+            d = {'type': 'slave', 'hostname': sys_info['hostname'], 'sys_info': sys_info}
+            if interactive:
+                print 'adding new node %s to database' % d['hostname']
+                enable = raw_input('Enable node? [True|False] ')
+                if enable == 'True':
+                    d['enabled'] = True
+                else:
+                    d['enabled'] = False
+                pw = raw_input('Node password? ')
+                d['password'] = pw
+            else:
+                d['enabled'] = node_enable_default
+                d['password'] = node_password_default
+        db.save(d)
+
 def system(cmd, wd=None):
     '''a wrapper for subprocess.call, which executes cmd in working directory
     wd in a bash shell, returning the exit code.'''
--- dirt.py
+++ dirt.py
@@ -4,80 +4,30 @@
 #
 # Andy Mastbaum (amastbaum@gmail.com), August 2011
 
-import couchdb
-import execnet
 import sys
 
-# put in config file
-couchdb_host = 'http://localhost:5984'
-couchdb_dbname = 'dirt-kanso'
-nodes = ['localhost']
-node_enable_default = True
-node_password_default = 'pw123'
-interactive = True
-
-# this should be a couchdb view
-def get_nodes(db):
-    nodedocs = []
-    hostnames = []
-    for id in db:
-        try:
-            if db[id]['type'] == 'slave':
-                print db[id]
-                nodedocs.append(db[id])
-                hostnames.append(db[id]['hostname'])
-        except KeyError:
-            pass
-    return hostnames, nodedocs
-
-def node_recon(nodes, db):
-    hostnames, nodedocs = get_nodes(db)
-    from tasks import system_info
-    for node in nodes:
-        gw = execnet.makegateway('ssh=%s' % node)
-        ch = gw.remote_exec(system_info)
-        sys_info = ch.receive()
-        print '==', node, '='*30
-        print sys_info
-        # update the db
-        if sys_info['hostname'] in hostnames:
-            d = nodedocs[hostnames.index(sys_info['hostname'])]
-            d['sys_info'] = sys_info
-        else:
-            d = {'type': 'slave', 'hostname': sys_info['hostname'], 'sys_info': sys_info}
-            if interactive:
-                print 'adding new node %s to database' % d['hostname']
-                enable = raw_input('Enable node? [True|False] ')
-                if enable == 'True':
-                    d['enabled'] = True
-                else:
-                    d['enabled'] = False
-                pw = raw_input('Node password? ')
-                d['password'] = pw
-            else:
-                d['enabled'] = node_enable_default
-                d['password'] = node_password_default
-        db.save(d)
-              
-
-def main():
+def serve_forever():
     print 'dirt is running...'
+    # do things forever
 
-    # connect to db
-    couch = couchdb.Server(couchdb_host)
-    try:
-        if couch.version() < '1.1.0':
-            print 'dirt: couchdb version >= 1.1.0 required'
-            sys.exit(1)
-        db = couch[couchdb_dbname]
-    except Exception:
-        print 'dirt: error connecting to database'
-        sys.exit(1)
-    print 'dirt: connected to db at %s/%s' % (couchdb_host, couchdb_dbname)
-
-    node_recon(nodes, db)
+def help():
+    print \
+'''
+usage:
+'''
 
 if __name__ == '__main__':
-    # some arguments?
-    main()
+    if len(sys.argv) < 2:
+        help()
+        sys.exit(1)
+
+    if sys.argv[1] == 'serve':
+        serve_forever()
+    elif sys.argv[1] == 'updatenodes':
+        import core.helpers
+        nodes = sys.argv[2:]
+        if len(nodes) == 0:
+            help()
+            sys.exit(1)
+        core.helpers.node_recon(nodes, core.helpers.connect_to_db())
 
