6d259de2c52d5e1e39f9eecae44cebb37f363f5d
Andy <amastbaum@gmail.com>
2011-09-02 19:11:21 -0400

    reimplemented task count and 'percent awesome' bar on index page

--- web/lib/views.js
+++ web/lib/views.js
@@ -3,8 +3,26 @@
  */
 exports.records_by_created = {
     map: function (doc) {
-        if (doc.type === 'record') {
-            emit(doc.id, {title: doc.title, description: doc.description});
+        if (doc.type == 'record') {
+            var percents = {'failed': 0, 'success': 0, 'inprogress': 0, 'waiting': 0};
+            for (tasknum in doc.tasks) {
+                task = doc.tasks[tasknum];
+                if (task.completed) {
+                    if (task.success)
+                        percents['success'] += 1;
+                    else
+                        percents['failed'] += 1;
+                }
+                else {
+                    if (task.checked_out)
+                        percents['inprogress'] += 1;
+                    else
+                        percents['waiting'] += 1;
+                }
+            }
+            for (item in percents)
+                percents[item] = percents[item] * 100 / doc.tasks.length;
+            emit(doc._id, {title: doc.title, description: doc.description, task_count: doc.tasks.length, percents: percents});
         }
     }
 };
